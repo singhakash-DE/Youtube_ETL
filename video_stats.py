9ba473f1cff609250973a6fc3e1d79b6c8162b4b
Akash <singhakash1269770@gmail.com>
2026-03-13 15:51:45 +0530

    Remove API key

--- video_stats.py
+++ video_stats.py
@@ -5,9 +5,10 @@ from dotenv  import load_dotenv
 
 load_dotenv(dotenv_path="./.env")
 
-#API_KEY = 'AIzaSyDlrbMsV52K_h63uL9fWRDpPfSBMZGkUB4'
+
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDEL = 'MrBeast'
+max_results = 50
 
 def get_playlsitID():
 
@@ -20,13 +21,36 @@ def get_playlsitID():
         channel_items = data["items"][0]
         channel_playlistId = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
 
-        #print(channel_playlistId)
+        print(channel_playlistId)
         return channel_playlistId
 
     except requests.exceptions.RequestException as e:
         raise e
     
 
+    base_url = f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={max_results}&playlistId={playlistId}&key=[YOUR_API_KEY]'
+    playlistId = get_playlsitID()
+
+def get_videoID(playlistId):
+    base_url = f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={max_results}&playlistId={playlistId}&key=[YOUR_API_KEY]'
+    video_id = []
+    pageToken = None
+
+    try:
+
+        while True:
+            url = base_url
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+            
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
 if __name__ == "__main__":
     print("get_playlistID will be exceuted")
-    playlist =get_playlsitID()
+    playlistId =get_playlsitID()

d28aa08f141e115ff48c40430d41d1cb6b0f0219
Akash <singhakash1269770@gmail.com>
2026-03-13 15:34:21 +0530

    Added .env file

---
+++ video_stats.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import os
+from dotenv  import load_dotenv
+
+load_dotenv(dotenv_path="./.env")
+
+#API_KEY = 'AIzaSyDlrbMsV52K_h63uL9fWRDpPfSBMZGkUB4'
+API_KEY = os.getenv("API_KEY")
+CHANNEL_HANDEL = 'MrBeast'
+
+def get_playlsitID():
+
+    try:
+        url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDEL}&key={API_KEY}'
+
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        channel_items = data["items"][0]
+        channel_playlistId = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
+
+        #print(channel_playlistId)
+        return channel_playlistId
+
+    except requests.exceptions.RequestException as e:
+        raise e
+    
+
+if __name__ == "__main__":
+    print("get_playlistID will be exceuted")
+    playlist =get_playlsitID()
